3c39104249b71a4fa0fa7e6a1e514b6a03ae2fa8
edunad <edunad@users.noreply.github.com>
2020-05-11 11:33:35 +0000

    Deploying to master from  @ 22e9ff727bc859a3a39a92e2c914651a6f9d9dd1 🚀

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import { App } from './components/App/App';
-
-ReactDOM.render(<App />, document.getElementById('app-container'));
